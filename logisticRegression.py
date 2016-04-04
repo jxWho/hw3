99da5d0706c89de076e52748f48eab5fc28dc16f
jh3648 <hjxwho@gmail.com>
2016-04-03 23:13:16 -0400

    part 1

--- logisticRegression.py
+++ logisticRegression.py
@@ -24,7 +24,7 @@ class LogisticRegression:
 
         self.params = [self.W, self.b]
 
-        raise NotImplementedError
+        #raise NotImplementedError
 
         #compile a theano function self.train that takes the matrix X, vector y, and learning rate as input
 
@@ -38,6 +38,43 @@ class LogisticRegression:
         # keep track of model input
         self.input = X
 
+        # cost function
+        cost = self.negative_log_likelihood(y)
+        # derivation
+        g_W = T.grad(cost=cost, wrt=self.W)
+        g_b = T.grad(cost=cost, wrt=self.b)
+        # how to update
+        updates = [(self.W, self.W - learning_rate * g_W),
+                   (self.b, self.b - learning_rate * g_b)]
+        # train model
+        index = T.lscalar()
+
+        self.train = theano.function(
+                inputs=[X, y, learning_rate],
+                updates=updates,
+                outputs = cost
+        )
+
+        # predcit model
+        self.predict = theano.function(
+                inputs=[self.input],
+                outputs=self.y_pred
+        )
+
+    # hack
+    def negative_log_likelihood(self, y):
+        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+
+    def errors(self, y):
+        if y.ndim != self.y_pred.ndim:
+            raise TypeError(
+                'y should have the same shape as self.y_pred',
+                ('y', y.type, 'y_pred', self.y_pred.type)
+            )
+        if y.dtype.startswith('int'):
+            return T.mean(T.neq(self.y_pred, y))
+        else:
+            raise NotImplementedError()
 
     def fit(self, X, y, num_epochs=5, learning_rate=0.05, verbose=False):
         for i in range(num_epochs):
@@ -45,4 +82,3 @@ class LogisticRegression:
             if verbose:
                 print('cost at epoch {}: {}'.format(i, cost))
 
-    
