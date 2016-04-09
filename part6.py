090f850a1bfe897997eb5418e11770c08f5bd210
jh3648 <hjxwho@gmail.com>
2016-04-08 21:32:04 -0400

    final part 6

--- part6.py
+++ part6.py
@@ -8,13 +8,29 @@ fileName = "random_init.npz"
 answerEmbedding = DependencyRNN.load(fileName).answers
 
 temp = []
+n = []
 for x in answerEmbedding:
     temp.append( answerEmbedding[x] )
+    n.append( x )
 
 X = np.asarray(temp)
 tsne = TSNE(n_components=2, perplexity=30.0)
 X_reduced = tsne.fit_transform(X)
 
-print X_reduced
+xs= []
+ys = []
+for x, y in X_reduced:
+    xs.append(x)
+    ys.append(y)
 
+fig, ax = plt.subplots()
+ax.scatter(xs, ys)
+for i, txt in enumerate(n):
+    ax.annotate(txt, (xs[i], ys[i]))
+
+plt.show()
+'''
 plt.plot(X_reduced)
+plt.ylabel('some numbers')
+plt.show()
+'''
